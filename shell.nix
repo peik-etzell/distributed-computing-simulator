e267932ede342062e5f66f6281ca0c561b9eb9f6
Peik Etzell <peik.etzell@gmail.com>
2023-11-25 09:23:56 +0200

    Simplify shell.nix

--- shell.nix
+++ shell.nix
@@ -2,19 +2,15 @@
 let
   python = pkgs.python311;
   pythonEnv = python.withPackages
-    (ps: with ps; [ pip virtualenvwrapper python-lsp-server ]);
-  lib-path = with pkgs; lib.makeLibraryPath [ libffi openssl stdenv.cc.cc ];
+    (ps: with ps; [ pip virtualenvwrapper ]);
 in pkgs.mkShell {
   packages = [ pythonEnv ];
   shellHook = ''
-    export "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${lib-path}"
-
     VENV=.venv
     if test ! -d $VENV; then
         virtualenv $VENV
     fi
     source ./$VENV/bin/activate
     export PYTHONPATH=`pwd`/$VENV/${python.sitePackages}/:$PYTHONPATH
-
   '';
 }
